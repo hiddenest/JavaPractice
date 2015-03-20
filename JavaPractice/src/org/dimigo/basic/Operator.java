fb8c2e943c8d1c8812c62095b7e9abec0f2c790d
Hiddenest <vison1641@naver.com>
2015-03-20 16:26:44 +0900

    세번째 과제수행

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,19 @@
+package org.dimigo.basic;
+
+public class Operator {
+	public static void main(String[] args) {
+		
+		double a=9, b=10, h1=5.8, h2=5.4;
+		double sq1 = (a+b)*h1/2;
+		double sq2 = a * h2;
+		
+		System.out.println("<< 도형 넓이 비교 >>");
+		System.out.println("사다리꼴 넓이 : " + sq1);
+		System.out.println("평행사변형 넓이 : " + sq2);
+		
+		if (sq1 > sq2)
+			System.out.println("\n사다리꼴이 평행사변형 보다 " + (sq1 - sq2) + " 더 큽니다.");
+		else
+			System.out.println("\n평행사변형이 사다리꼴 보다 " + (sq2 - sq1) + " 더 큽니다.");
+	}
+}
