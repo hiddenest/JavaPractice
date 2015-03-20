c0672266ede89373004a8123c7bf49412348260f
Hiddenest <vison1641@naver.com>
2015-03-20 16:29:29 +0900

    세번째 과제수행

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -3,14 +3,17 @@ package org.dimigo.basic;
 public class Operator {
 	public static void main(String[] args) {
 		
+		// 귀찮지만 언제나 그랬든 하는 변수 선언
 		double a=9, b=10, h1=5.8, h2=5.4;
-		double sq1 = (a+b)*h1/2;
-		double sq2 = a * h2;
+		double sq1 = (a+b)*h1/2; //사다리꼴
+		double sq2 = a * h2; //평행사변형
 		
+		//그리고 출력
 		System.out.println("<< 도형 넓이 비교 >>");
 		System.out.println("사다리꼴 넓이 : " + sq1);
 		System.out.println("평행사변형 넓이 : " + sq2);
 		
+		//크기비교
 		if (sq1 > sq2)
 			System.out.println("\n사다리꼴이 평행사변형 보다 " + (sq1 - sq2) + " 더 큽니다.");
 		else
