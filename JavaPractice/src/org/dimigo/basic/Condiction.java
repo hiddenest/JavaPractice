897fa69927e295326ab33c2a25aa2e24bb467df3
Hiddenest <vison1641@naver.com>
2015-03-24 15:14:01 +0900

    네번째 수행평가 과제

--- JavaPractice/src/org/dimigo/basic/Condiction.java
+++ JavaPractice/src/org/dimigo/basic/Condiction.java
@@ -2,14 +2,18 @@ package org.dimigo.basic;
 
 public class Condiction {
 	public static void main(String[] args) {
+		
+		// 꼼수용 변수선언
 		int distance = 15;
 		int defaultCharge = 0;
 		String transportation = "고속버스";
 		
+		//기본 출력
 		System.out.println("<< 고속도로 통행료 계산 >>");
 		System.out.println("거리 : " + distance + "km");
 		System.out.println("차종 : " + transportation);
 		
+		//스위치-케이스 계산
 		switch(transportation) {
 		case "고속버스":
 			defaultCharge = 850 + ((distance-1) / 10 * 300);
@@ -22,6 +26,7 @@ public class Condiction {
 			break;
 		}
 		
+		//통행료 출력
 		System.out.println("통행료 : " + defaultCharge +"원");
 	}
 }
