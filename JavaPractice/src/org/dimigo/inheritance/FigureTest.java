af5945b5427c61767a6cf25f706102477353194c
이찬희 <Hidden@172.17.1.251>
2015-08-12 10:52:16 +0900

    lkhjg

--- JavaPractice/src/org/dimigo/inheritance/FigureTest.java
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -22,7 +22,7 @@ public class FigureTest {
 		Triangle t = new Triangle(10, 10, 5, 8);
 		Rectangle r = new Rectangle(20, 20, 5, 8);
 		
-		System.out.println("원의 넓이 : " + c.calcArea());
+		System.out.println("원의 넓이 : " + String.format("%.1f",c.calcArea()));
 		System.out.println("삼각형의 넓이 : " + t.calcArea());
 		System.out.println("사각형의 넓이 : " + r.calcArea());
 		
