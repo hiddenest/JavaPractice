3f7534f718505e2c30988e4f13e2cdfb9a5ad0f3
이찬희 <Hidden@172.17.1.31>
2015-08-26 09:22:17 +0900

    실습과제 2

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * 		|_ SmartPhone
+ * 
+ * 1. 개요 :		
+ * 2. 작성일 :		2015. 8. 25.
+ * </pre>
+ * 
+ * @author :		이찬희
+ * @version:		1.0
+ *
+ */
+public class SmartPhone {
+	private String model; //모델
+	private String company; //제조사
+	private int price; //가격
+	
+	public SmartPhone() { //더미생성자
+	}
+
+	public SmartPhone(String model, String company, int price) {
+		super();
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public void pay() { //어차피 오버라이딩할꺼니까 비워놓고
+	}
+	
+	public void useSpecialFunction(SmartPhone phone) {
+		if (phone == null) {
+			return ; //널체크
+		}
+		else if (phone instanceof Galaxy) {
+			Galaxy g = (Galaxy) phone; //캐스팅
+			g.useWirelessCharge();
+		}
+		else {
+			IPhone i = (IPhone) phone; //캐스팅
+			i.useAirDrop();
+		}
+	}
+	
+	public String toString() {
+		String output = new String("모델명 : " + model + ", 제조사 : " + company + ", 가격 : "); //StringBuilder 쓸걸
+		output += String.format("%,1d", price) + "원";
+		
+		return output;
+	}
+	
+	
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * 		|_ SmartPhoneTest
+ * 
+ * 1. 개요 :		
+ * 2. 작성일 :		2015. 8. 25.
+ * </pre>
+ * 
+ * @author :		이찬희
+ * @version:		1.0
+ *
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone i = new IPhone("iPhone 6", "애플", 700000); //사과
+		SmartPhone g = new Galaxy("갤럭시 S6", "삼성", 650000); //샘숭
+		
+		System.out.println(i.toString()); //일단 출력
+		i.turnOn(); //켜고
+		i.pay(); //애플페이
+		i.useSpecialFunction(i); //에어드롭
+		i.turnOff(); //끄고
+		
+		System.out.println();
+		
+		System.out.println(g.toString()); //출력
+		g.turnOn(); //켜고
+		g.pay(); //삼성페이(?)
+		g.useSpecialFunction(g); //무선충전(?)
+		g.turnOff(); //끄고
+	}
+}
